6759f38e17018585d2b57fa8bf343cfaf5f1f89f
Yash <yashp241195@gmail.com>
2017-12-04 01:14:37 +0530

    Update all_factors.py

--- Math/all_factors.py
+++ Math/all_factors.py
@@ -16,3 +16,15 @@ class Solution:
                     L.append(int(A/i))
         L.sort()
         return(L)
+
+    # @param A : integer
+    # @return an integer
+    def isPrime(self, A):
+        # 1 is not a prime number
+        if A == 1:
+            return 0
+    	upperLimit = int(A**0.5)
+    	for i in xrange(2, upperLimit + 1):
+    		if i < A and A % i == 0:
+    			return 0
+    	return 1
