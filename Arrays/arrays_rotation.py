817d6dbe3f36dac5d8ec35a76eab4c52f2d46b06
Yash <yashp241195@gmail.com>
2017-12-27 03:21:31 +0530

    Update arrays_rotation.py

--- Arrays/arrays_rotation.py
+++ Arrays/arrays_rotation.py
@@ -89,3 +89,70 @@ def rotate(array, k):
 print("Array : ", arr)
 arr = rotate(arr, 3)
 print("After Rotation by ", 3, " Array : ", arr)
+
+# Binary Search in rotated Array
+
+arr = [4, 5, 6, 7, 8, 9, 1, 2, 3]
+
+
+def binary_search(array, key, left, right):
+    if left < right:
+        mid = int((right + left)/2)
+        if key == array[mid]:
+            return mid
+        elif key < array[mid]:
+            return binary_search(array, key, left, mid)
+        elif key > array[mid]:
+            return binary_search(array, key, mid+1, right)
+
+
+def rotated_binary_search(array, key, left, right, pivot):
+    if key == array[pivot]:
+        return pivot
+    elif key == array[pivot - 1]:
+        return pivot - 1
+    elif key == array[left]:
+        return left
+    elif key == array[right]:
+        return right
+    else:
+        if key > array[left]:
+            return binary_search(array, key, left, pivot-1)
+        else:
+            return binary_search(array, key, pivot, right)
+
+
+def find_pivot(array, left, right):
+    mid = int((right + left)/2)
+    if array[mid] > array[mid+1]:
+        return mid + 1
+    else:
+        if array[left] > array[mid]:
+            return find_pivot(array, left, mid - 1)
+        else:
+            return find_pivot(array, mid + 1, right)
+
+
+print(arr)
+last = len(arr)-1
+pivot = find_pivot(arr, 0, last)
+
+print("Pivot Index : ", pivot, "Pivot Element : ", arr[pivot])
+# for verification :
+for i in range(last+1):
+    key2 = arr[i]
+    index = rotated_binary_search(arr, key2, 0, last, pivot)
+    print("key : ",key2," Verified Key : ",arr[index])
+
+
+
+
+
+
+
+
+
+
+
+
+
