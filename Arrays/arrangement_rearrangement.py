b96d1f609f019043ccc3968c50bc8252261a9114
Yash <yashp241195@gmail.com>
2017-12-31 04:44:30 +0530

    Update arrangement_rearrangement.py

--- Arrays/arrangement_rearrangement.py
+++ Arrays/arrangement_rearrangement.py
@@ -198,7 +198,30 @@ for i in range(size):
 print("BTS length", mx)
 
 
+# Min max array (Using modulo arithmetic)
 
+arr = [1, 2, 3, 4, 5, 9]
+size = len(arr)
+
+minIndex = 0
+maxIndex = size - 1
+
+maxim = max(arr) + 1
+print(arr)
+
+for i in range(size):
+    if i % 2 == 0:
+        arr[i] += (arr[maxIndex] % maxim) * maxim
+        maxIndex -= 1
+    else:
+        arr[i] += (arr[minIndex] % maxim) * maxim
+        minIndex += 1
+
+# Reforming the array
+for i in range(size):
+    arr[i] = int(arr[i]/maxim)
+
+print(arr)
 
 
 
