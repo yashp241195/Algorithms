89688376b2440456a5df979edc292d38272b0ae9
Yash <yashp241195@gmail.com>
2018-01-11 10:51:18 +0530

    Update fibonacci.py

--- Dynamic_Programming/fibonacci.py
+++ Dynamic_Programming/fibonacci.py
@@ -1,3 +1,4 @@
+
 # Dynamic Programming FAST method
 # Fibonacci Problem
 
@@ -43,4 +44,49 @@ print("\nvalue of ", num, " th sequence of fibonacci is ", seq, "\n")
 
 for i in range(num):
     print(i, " is repeated / overlapped ", counting[i], " times")
+    
+# Making Dynamic Programming approach
+
+cache = [-1] * (num + 1)
+
+
+def fib_dp(n, cache):
+    if n == 0 or n == 1:
+        cache[n] = n
+        return n
+    elif cache[n] != -1:
+        return cache[n]
+    else:
+        cache[n] = fib_dp(n-1, cache) + fib_dp(n-2, cache)
+        return cache[n]
+
+
+seq = fib_dp(num, cache)
+print("\nValue of ", num+1, " th sequence or fibonacci(", num, ")  of fibonacci is ", seq, "\n")
+print(cache)
+
+
+# Turn the solution around :
+# previously, we were building the solution
+# from fib 6 -> fib 5 -> fib 4 -> fib 3 .... fib 0
+# but now we will build it from bottom up
+# fib 0 -> fib 1 -> fib 2 ... fib 6..
+
+cache = [1] * (num + 1)
+
+
+def fib_turn_around(n, cache):
+    cache[0] = 0
+    cache[1] = 1
+
+    for i in range(2, n+1):
+        cache[i] = cache[i-1] + cache[i-2]
+
+    return cache[n]
+
+
+seq = fib_turn_around(num, cache)
+print("\nValue of ", num+1, " th sequence or fibonacci(", num, ")  of fibonacci is ", seq, "\n")
+print(cache)
+
 
